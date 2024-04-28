b032156a46fd04c98295bd543b365bce6c8dec0a
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-04-28 15:36:23 -0700

    Optimize #345

--- 0000/0345-reverse-vowels-of-a-string.ts
+++ 0000/0345-reverse-vowels-of-a-string.ts
@@ -1,4 +1,5 @@
-function reverseVowels(s: string): string {
+// First attempt
+function reverseVowels1(s: string): string {
   const vowels = new Set<string>(['a', 'e', 'i', 'o', 'u']);
   let letters = s.split("");
   let reversedVowels: string[] = [];
@@ -14,6 +15,27 @@ function reverseVowels(s: string): string {
       if (vowels.has(letters[j].toLowerCase())) {
           letters[j] = reversedVowels[currentVowel];
           currentVowel += 1;
+      }
+  }
+
+  return letters.join("");
+};
+
+// Optimized attempt, using complete set and stack data structure
+function reverseVowels2(s: string): string {
+  const vowels = new Set<string>(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  let letters = s.split("");
+  let reversedVowels: string[] = [];
+
+  for (let i=0; i<letters.length; i++) {
+      if (vowels.has(letters[i])) {
+          reversedVowels.push(letters[i]);
+      }
+  }
+
+  for (let j=0; j<letters.length; j++) {
+      if (vowels.has(letters[j])) {
+          letters[j] = reversedVowels.pop();
       }
   }
 
