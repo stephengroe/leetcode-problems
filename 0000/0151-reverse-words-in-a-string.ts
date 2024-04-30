3d3a2ccbc6ff7776c485da8b29816d24efa5bf73
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-04-29 20:51:03 -0700

    Add new solution to #151

--- 0000/0151-reverse-words-in-a-string.ts
+++ 0000/0151-reverse-words-in-a-string.ts
@@ -7,3 +7,28 @@ function reverseWords1(s: string): string {
 function reverseWords2(s: string): string {
   return s.split(" ").filter(Boolean).reverse().join(" ");
 };
+
+// Version 3, using accumulated word string
+function reverseWords(s: string): string {
+  let currentWord = "";
+  let result: string[] = [];
+
+  for (let i=0; i<s.length; i++) {
+      if (s[i] === " ") {
+          if (currentWord) {
+              result.push(currentWord);
+              currentWord = "";
+          } else {
+              continue;
+          }
+      } else {
+          currentWord += s[i];
+
+          if (i === s.length - 1) {
+              result.push(currentWord);
+          }
+      }
+  }
+
+  return result.reverse().join(" ");
+}
