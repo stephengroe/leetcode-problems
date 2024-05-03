f069152bc64b76aee7374859a15cb838dc8c7f27
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-02 21:45:22 -0700

    Add second attempt to #1456

--- 1000/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
+++ 1000/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -1,5 +1,5 @@
 // First attempt, not efficient enough to pass
-function maxVowels(s: string, k: number): number {
+function maxVowels1(s: string, k: number): number {
   const letters = s.split('');
   const vowels = new Set<string>(['a', 'e', 'i', 'o', 'u']);
   let window: number[] = [];
@@ -25,4 +25,34 @@ function maxVowels(s: string, k: number): number {
   }
 
   return max;    
+};
+
+// Second attempt, real version of sliding window this time!
+
+function maxVowels2(s: string, k: number): number {
+  const letters = s.split('');
+  const vowels = new Set<string>(['a', 'e', 'i', 'o', 'u']);
+
+  let left = 0;
+  let currentCount = 0;
+  let maxCount = 0;
+
+  for (let right=0; right<letters.length; right++) {
+      // Increment if window contains vowel
+      if (vowels.has(letters[right])) {
+          currentCount += 1;
+      }
+
+      // Decrement if vowel leaves window
+      if ((right - left) >= k) {
+          if (vowels.has(letters[left])) {
+              currentCount -= 1;
+          }
+          left += 1;
+      }
+
+      maxCount = Math.max(maxCount, currentCount);
+  }
+
+  return maxCount;    
 };
