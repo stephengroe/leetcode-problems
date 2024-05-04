7c1cf2e0b0dfce00cc487b08dce9880ae30bcded
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-03 19:57:39 -0700

    Add second solution to #1679

--- 1000/1679-max-number-of-k-sum-pairs.ts
+++ 1000/1679-max-number-of-k-sum-pairs.ts
@@ -17,3 +17,31 @@ function maxOperations1(nums: number[], k: number): number {
 
   return total;
 };
+
+// Second solution: Optimized with Map
+function maxOperations2(nums: number[], k: number): number {
+  let total = 0;
+  let pairs = new Map<number, number>();
+
+  for (let i=0; i<nums.length; i++) {
+      const num = nums[i];
+
+      if (pairs.has(num)) {
+          total += 1;
+          if (pairs.get(num) === 1) {
+              pairs.delete(num);
+          } else {
+              pairs.set(num, pairs.get(num) - 1);
+          }
+      } else {
+          const pair = k - num;
+          if (pairs.has(pair)) {
+              pairs.set(pair, pairs.get(pair) + 1);
+          } else {
+              pairs.set(pair, 1);
+          }
+      }
+  }
+
+  return total;
+};
