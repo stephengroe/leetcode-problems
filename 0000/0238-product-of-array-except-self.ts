82356106d4cec05ad0928a1da02aea306687ff84
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-06 20:29:39 -0700

    Add optimized solution to #238

--- 0000/0238-product-of-array-except-self.ts
+++ 0000/0238-product-of-array-except-self.ts
@@ -11,3 +11,22 @@ function productExceptSelf1(nums: number[]): number[] {
 
   return result;
 };
+
+// Optimized solution
+function productExceptSelf(nums: number[]): number[] {
+  let result = new Array(nums.length).fill(1);
+
+  let prefix = 1;
+  for (let i=0; i<nums.length; i++) {
+      result[i] *= prefix;
+      prefix *= nums[i];
+  }
+
+  let postfix = 1;
+  for (let i=nums.length - 1; i>=0; i--) {
+      result[i] *= postfix;
+      postfix *= nums[i];
+  }
+
+  return result;
+};
