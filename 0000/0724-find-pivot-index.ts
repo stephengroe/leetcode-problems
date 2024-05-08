e2ec591027154b7aecfd8d1eaf385e763f40b535
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-08 00:29:18 -0700

    Add second solution to #724

--- 0000/0724-find-pivot-index.ts
+++ 0000/0724-find-pivot-index.ts
@@ -1,5 +1,5 @@
 // First solution
-function pivotIndex(nums: number[]): number {
+function pivotIndex1(nums: number[]): number {
   let prefix = nums.slice();
   let postfix = nums.slice();
 
@@ -15,6 +15,22 @@ function pivotIndex(nums: number[]): number {
       let left = prefix[i-1] || 0;
       let right = postfix[i+1] || 0;
       if (left === right) return i;
+  }
+
+  return -1;
+};
+
+// Second solution with memory O(1)
+function pivotIndex(nums: number[]): number {
+  let left = 0, right = nums.reduce((a, b) => a + b);
+
+  for (let i=0; i<nums.length; i++) {
+      right -= nums[i];
+      if (left === right) {
+          return i;
+      } else {
+          left += nums[i];
+      }
   }
 
   return -1;
