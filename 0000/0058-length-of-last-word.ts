80c268522a062660b2eb769ea4cfe339b1fda3cc
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-09 23:05:57 -0700

    Add second solution to #58

--- 0000/0058-length-of-last-word.ts
+++ 0000/0058-length-of-last-word.ts
@@ -1,5 +1,10 @@
 // First solution
-function lengthOfLastWord(s: string): number {
+function lengthOfLastWord1(s: string): number {
   const wordArray = s.trim().split(" ");
   return wordArray[wordArray.length - 1].length;    
+};
+
+// Second solution, simplified to a single line!
+function lengthOfLastWord2(s: string): number {
+  return s.trim().split(" ").pop().length;
 };
