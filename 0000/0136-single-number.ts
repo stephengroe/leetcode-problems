c1c345fa18de4574cd334748b3517b8625a3e8d0
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-24 20:44:44 -0700

    Add second solution to #136

--- 0000/0136-single-number.ts
+++ 0000/0136-single-number.ts
@@ -1,5 +1,6 @@
-// First solution - time: O(n), space: O(n)
-function singleNumber(nums: number[]): number {
+// First solution with hash set
+// Time: O(n), Space: O(n)
+function singleNumber1(nums: number[]): number {
   let set = new Set<number>();
 
   for (let i=0; i<nums.length; i++) {
@@ -11,4 +12,16 @@ function singleNumber(nums: number[]): number {
   }
 
   return set.values().next().value;  
+};
+
+// Second solution, bit manipulation with XOR
+// Time: 0(n), Space: O(1)
+function singleNumber2(nums: number[]): number {
+  let result = 0;
+
+  for (let i=0; i<nums.length; i++) {
+      result = result ^ nums[i];
+  }
+
+  return result;  
 };
