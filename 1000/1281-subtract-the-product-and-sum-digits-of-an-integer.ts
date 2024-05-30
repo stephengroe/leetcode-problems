9b0e8e2b01b233d76bb3a37b559cd3cdf2df274c
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-05-29 19:34:39 -0700

    Add second solution to #1281

--- 1000/1281-subtract-the-product-and-sum-digits-of-an-integer.ts
+++ 1000/1281-subtract-the-product-and-sum-digits-of-an-integer.ts
@@ -1,6 +1,24 @@
 // First solution, brute force
 // Time: O(n), space: O(n)
-function subtractProductAndSum(n: number): number {
+function subtractProductAndSum1(n: number): number {
   let numArray = n.toString().split("").map(n => Number(n));
   return numArray.reduce((x, y) => x * y) - numArray.reduce((x, y) => x + y);
+};
+
+// Second solution, single loop
+// Time: O(n), space: O(1)
+function subtractProductAndSum2(n: number): number {
+  const length = n.toString().length;
+  let int = n;
+  let product = 1;
+  let sum = 0;
+
+  for (let i=0; i<length; i++) {
+      let currentNum = int % 10;
+      product *= currentNum;
+      sum += currentNum;
+      int = Math.floor(int / 10);
+  }
+
+  return product - sum;
 };
