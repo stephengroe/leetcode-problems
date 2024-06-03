5dbc508e77ddff7c2797541c3324fbff25524bab
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-06-02 18:01:26 -0700

    Add new solution to #2486: Append characters to string to make subsequence

--- 2000/2486-append-characters-to-string-to-make-subsequence.ts
+++ 2000/2486-append-characters-to-string-to-make-subsequence.ts
@@ -1,5 +1,6 @@
-// First solution
-function appendCharacters(s: string, t: string): number {
+// First solution (solved 2024-05-03)
+// Time: O(n), space: O(1)
+function appendCharacters1(s: string, t: string): number {
   let i = 0;
   let j = 0;
 
@@ -12,4 +13,22 @@ function appendCharacters(s: string, t: string): number {
   }
 
   return t.length - j;
+};
+
+// Second solution (solved 2024-06-02)
+// Time: O(n), space: O(1)
+function appendCharacters2(s: string, t: string): number {
+  let maxLength = 0;
+  let currentLength = 0;
+  let tMarker = 0;
+
+  for (let i=0; i<s.length; i++) {
+      if (s[i] === t[tMarker]) {
+          currentLength += 1;
+          maxLength = Math.max(maxLength, currentLength);
+          tMarker += 1;
+      }
+  }
+
+  return t.length - maxLength;
 };
