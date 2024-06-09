1d5382d241e2c1c2224de07851bc5399d5e6d575
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-06-08 21:55:31 -0700

    Add #2723: Add Two Promises

---
+++ 2000/2723-add-two-promises.ts
@@ -0,0 +1,12 @@
+// First solution, 2024-06-08
+// Time: O(1), space: O(1)
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    return await promise1 + await promise2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
