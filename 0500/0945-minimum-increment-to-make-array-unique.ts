695682f10ad7745b91842e9e29706a2dd2382cbb
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-06-14 00:08:16 -0400

    Add new solution to #945

--- 0500/0945-minimum-increment-to-make-array-unique.ts
+++ 0500/0945-minimum-increment-to-make-array-unique.ts
@@ -1,6 +1,6 @@
 // Brute force solution, solved 2024-06-13
 // Time: O(n^2), space: O(n)
-function minIncrementForUnique(nums: number[]): number {
+function minIncrementForUnique1(nums: number[]): number {
   let uniqueNums = new Set<number>();
   let sortedNums = nums.sort((a, b) => a - b);
   let moves = 0;
@@ -14,4 +14,28 @@ function minIncrementForUnique(nums: number[]): number {
   }
 
   return moves;    
+};
+
+// Second solution, optimized
+// Time: O(n log n), space: O(n)
+function minIncrementForUnique2(nums: number[]): number {
+  let uniqueNums = new Set<number>();
+  let sortedNums = nums.sort((a, b) => a - b);
+  let moves = 0;
+  let nextAvailable = -Infinity;
+
+  for (let i=0; i<sortedNums.length; i++) {
+      let num = sortedNums[i];
+
+      if (uniqueNums.has(num)) {
+          moves += (nextAvailable - num);
+          uniqueNums.add(nextAvailable);
+          nextAvailable += 1;
+      } else {
+          uniqueNums.add(num);
+          nextAvailable = num + 1;
+      }
+  }
+
+  return moves;
 };
