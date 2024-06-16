709dfb0d97c49010f584cf9376d60b1415ff18de
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-06-16 17:11:14 -0400

    Update #330

--- 0000/0330-patching-array.ts
+++ 0000/0330-patching-array.ts
@@ -1,20 +1,14 @@
 // First solution, solved 2024-06-16 (with help)
 // Time: O(n), space: O(1)
-
 function minPatches(nums: number[], n: number): number {
   let count = 0;
   let currentSum = 0;
   let pointer = 0;
 
   while (currentSum < n) {
-      if (pointer < nums.length) {
-          if (nums[pointer] === currentSum + 1) {
+      if (pointer < nums.length && nums[pointer] <= currentSum + 1) {
               currentSum += nums[pointer];
-          } else {
-              currentSum += currentSum + 1;
-              count += 1;
-          }
-          pointer += 1;
+              pointer += 1;
       } else {
           currentSum += currentSum + 1;
           count += 1;
