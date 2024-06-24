bc1157453ec1afa133fc254a94a49e45bc1153a2
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-06-24 13:14:24 -0700

    Add second solution for #995

--- 0500/995-minimum-number-of-k-consecutive-bit-flips.ts
+++ 0500/995-minimum-number-of-k-consecutive-bit-flips.ts
@@ -1,7 +1,7 @@
 // First solution, solved 2024-06-24 (with some help)
 // Time: O(n * k), space: O(n)
 // (Hits Time Limit Exceeded)
-function minKBitFlips(nums: number[], k: number): number {
+function minKBitFlips1(nums: number[], k: number): number {
   let bits = nums.slice();
   let count = 0;
 
@@ -17,7 +17,6 @@ function minKBitFlips(nums: number[], k: number): number {
               }});
           bits.splice(i, k, ...flippedBits);
       }
-      console.log(bits);
   }
 
   // If we were unable to flip any
@@ -26,4 +25,28 @@ function minKBitFlips(nums: number[], k: number): number {
   } else {
       return count;
   }
+};
+
+// Second solution, solved 2024-06-24 (with more help)
+// Time: O(n), space: O(k)
+function minKBitFlips2(nums: number[], k: number): number {
+  let queue: number[] = [];
+  let count = 0;
+
+  for (let i=0; i<nums.length; i++) {
+      while (queue && i > queue[0] + k - 1) {
+          queue.shift();
+      }
+
+      if ((nums[i] + queue.length) % 2 === 0) {
+          if ((i + k) > nums.length) {
+              return -1;
+          } else {
+              count += 1;
+              queue.push(i);
+          }
+      }
+  }
+
+  return count;
 };
