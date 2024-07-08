fc492c9ad2bd3adbc4affb4b64e1138ae85ccd98
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-07-07 18:08:18 -0700

    Corrected time complexity on #1823

--- 1500/1823-find-the-winner-of-the-circular-game.ts
+++ 1500/1823-find-the-winner-of-the-circular-game.ts
@@ -1,5 +1,5 @@
 // First (naive) solution, solved 2024-07-07 in 48:06 (with some help)
-// Time: O(n), space: O(n)
+// Time: O(n * k), space: O(n)
 function findTheWinner(n: number, k: number): number {
   let friendQueue: number[] = [];
   for (let i=1; i<=n; i++) {
