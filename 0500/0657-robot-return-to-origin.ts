5642c103593307a50ea7011a2c915dfb9fa1da42
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-07-31 20:05:33 -0700

    Add third solution to #657

--- 0500/0657-robot-return-to-origin.ts
+++ 0500/0657-robot-return-to-origin.ts
@@ -33,3 +33,19 @@ function judgeCircle2(moves: string): boolean {
 
     return (x === 0 && y === 0);
 }
+
+// Third solution, optimized again
+function judgeCircle3(moves: string): boolean {
+  if (moves.length % 2 !== 0) return false;
+
+let x = 0, y = 0;
+
+for (let move of moves) {
+  if (move === 'R') x += 1;
+  else if (move === 'L') x -= 1;
+  else if (move === 'U') y += 1;
+  else if (move === 'D') y -= 1;
+}
+
+  return (x === 0 && y === 0);
+}
